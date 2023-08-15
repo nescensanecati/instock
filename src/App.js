2f3dc00ae476ae0c465bb15b412a2c04ce95787d
mariavgomez <mvicoria_g23@hotmail.com>
2023-08-15 16:36:28 -0400

    creating the initial folder structure and routes

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.scss';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/HomePage/Home';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>   
+          <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+    </BrowserRouter> 
+    </>
+  );
+}
+
+export default App;
