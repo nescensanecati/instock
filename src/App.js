97d89b9279dc8741f91cc4f423b1c9502174c0fe
Alejandro Malagon Diaz <amd@outlook.es>
2023-08-18 23:55:02 -0400

    fixing some stuff

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import Deleteinventory from "./component/Deleteinventory/Deleteinventory";
-import Deletewarehouse from "./component/Deletewarehouse/Deletewarehouse";
+import Deleteinventory from "./components/Deleteinventory/Deleteinventory";
+import Deletewarehouse from "./components/Deletewarehouse/Deletewarehouse";
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
 
